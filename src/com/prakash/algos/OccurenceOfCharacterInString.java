f3e95d35a2b2fd5fb18b4140cdef43d25e7165c5
prakashbtech87 <prakashbtech87@gmail.com>
2021-09-12 20:33:14 +0530

    Java First Commit Character Occurence

---
+++ src/com/prakash/algos/OccurenceOfCharacterInString.java
@@ -0,0 +1,32 @@
+package com.prakash.algos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccurenceOfCharacterInString {
+
+	public static void charCount(String str) {
+
+		Map<Character, Integer> charCountMap = new HashMap<>();
+
+		char[] charArray = str.toCharArray();
+
+		for (char c : charArray) {
+
+			if (charCountMap.containsKey(c)) {
+				charCountMap.put(c, charCountMap.get(c) + 1);
+			} else {
+				charCountMap.put(c, 1);
+			}
+
+		}
+		for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+	}
+
+	public static void main(String[] args) {
+		OccurenceOfCharacterInString.charCount("Java");
+	}
+
+}
