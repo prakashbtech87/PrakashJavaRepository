89384d10fe4620ab648ddd03dcb08a0145cb9609
prakashbtech87 <prakashbtech87@gmail.com>
2021-09-13 18:36:20 +0530

    Array Demo

---
+++ src/com/prakash/arrayz/ArrayDemo.java
@@ -0,0 +1,18 @@
+package com.prakash.arrayz;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		int arr[] = new int[2];
+
+		arr[0] = 1;
+		arr[1] = 2;
+
+		for (int ii : arr) {
+			System.out.println(ii);
+		}
+
+	}
+
+}
