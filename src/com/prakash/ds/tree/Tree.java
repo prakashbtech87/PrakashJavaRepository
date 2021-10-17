cef00cd0a4b095f8b1d22c0989e0063f7b575d4e
prakashbtech87 <prakashbtech87@gmail.com>
2021-10-17 18:02:26 +0530

    BST INSERT/TRAVERSE/FIND/MIN/MAX

--- src/com/prakash/ds/tree/Tree.java
+++ src/com/prakash/ds/tree/Tree.java
@@ -12,4 +12,32 @@ public class Tree {
 		}
 	}
 
+	public void traverseInOrder() {
+		if (root != null) {
+			root.traverseInOrder();
+		}
+	}
+
+	public TreeNode get(int value) {
+		if (root != null) {
+			return root.get(value);
+		}
+		return null;
+	}
+
+	public int min() {
+		if (root == null) {
+			return Integer.MIN_VALUE;
+		} else {
+			return root.min();
+		}
+	}
+
+	public int max() {
+		if (root == null) {
+			return Integer.MAX_VALUE;
+		} else {
+			return root.max();
+		}
+	}
 }
