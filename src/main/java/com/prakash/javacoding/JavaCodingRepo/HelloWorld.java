19730619a222b6635eb3fafdaaad07d10a15f84e
prakashkaruppusamy <prakashbtech87@gmail.com>
2024-07-06 20:38:56 +0530

    Test Commmit

---
+++ src/main/java/com/prakash/javacoding/JavaCodingRepo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.prakash.javacoding.JavaCodingRepo;
+
+/**
+ * @author prakashkaruppusamy
+ */
+public class HelloWorld {
+    public static void main(String[] ag) {
+        System.out.println("Hello World");
+    }
+}
