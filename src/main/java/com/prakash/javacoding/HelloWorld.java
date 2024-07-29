621c050ac2da676dbda01cc7133749a3dab11ae0
Prakash Karuppusamy <35074120+prakashbtech87@users.noreply.github.com>
2024-07-29 21:03:06 +0530

    Update HelloWorld.java

--- src/main/java/com/prakash/javacoding/HelloWorld.java
+++ src/main/java/com/prakash/javacoding/HelloWorld.java
@@ -4,7 +4,7 @@ package com.prakash.javacoding;
  * @author prakashkaruppusamy
  */
 public class HelloWorld {
-    public static void main(String[] ag) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
