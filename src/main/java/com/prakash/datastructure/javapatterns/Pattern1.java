e27a1b9054d20954fe871ea56dbe86cf067b7a43
Prakash Karuppusamy <35074120+prakashbtech87@users.noreply.github.com>
2024-10-19 20:54:39 +0530

    Update Pattern1.java

--- src/main/java/com/prakash/datastructure/javapatterns/Pattern1.java
+++ src/main/java/com/prakash/datastructure/javapatterns/Pattern1.java
@@ -1,1 +1,21 @@
+package com.prakash.datastructure.javapatterns;
+
+public class Pattern1 {
+    
+
+    public static void main(String[] args) {
+
+        printPattern1(5);
+
+    }
+
+    public static void printPattern1(int n) {
+        for (int outerloop = 0; outerloop < n; outerloop++) {
+            for (int innerloop = 0; innerloop < n; innerloop++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
 
