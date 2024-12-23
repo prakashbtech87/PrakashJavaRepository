5453ff75a1109e5f87c26f45f187f05460f9d9ab
prakashkaruppusamy <prakashbtech87@gmail.com>
2024-12-23 16:44:19 +0530

    Reverse Number

--- src/main/java/com/prakash/a2zdsa/ReverseOfNumber.java
+++ src/main/java/com/prakash/a2zdsa/ReverseOfNumber.java
@@ -1,10 +1,24 @@
 package com.prakash.a2zdsa;
 
+import java.util.Scanner;
+
 /**
  * @author prakashkaruppusamy
  */
 public class ReverseOfNumber {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int n = scanner.nextInt();
+        int ld = 0;
+        int rev = 0;
+
+        while (n > 0) {
+            ld = n % 10;
+            rev = rev * 10 + ld;
+            n = n / 10;
+        }
+        System.out.println(rev);
 
     }
 }
