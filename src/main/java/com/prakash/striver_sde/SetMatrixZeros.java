2899152f9ac608931129e063541cd7fb4d826db2
prakashkaruppusamy <prakashbtech87@gmail.com>
2025-05-31 16:08:51 +0530

    mark zeros

--- src/main/java/com/prakash/striver_sde/SetMatrixZeros.java
+++ src/main/java/com/prakash/striver_sde/SetMatrixZeros.java
@@ -5,31 +5,5 @@ package com.prakash.striver_sde;
  */
  class SetMatrixZeroes {
 
-    public void setZeroes(int[][] matrix) {
-        int rowCount = matrix.length;
-        int colCount = matrix[0].length;
-
-        // Step 1: Create helper arrays
-        boolean[] rows = new boolean[rowCount];
-        boolean[] cols = new boolean[colCount];
-
-        // Step 2: Mark rows and columns that need to be zeroed
-        for (int i = 0; i < rowCount; i++) {
-            for (int j = 0; j < colCount; j++) {
-                if (matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
-                }
-            }
-        }
-
-        // Step 3: Set elements to zero based on marked rows and columns
-        for (int i = 0; i < rowCount; i++) {
-            for (int j = 0; j < colCount; j++) {
-                if (rows[i] || cols[j]) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
+    
 }
